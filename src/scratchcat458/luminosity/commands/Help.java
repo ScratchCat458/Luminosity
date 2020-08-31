8f38027c3075949ff4c18887e7d6fcb6b0d52e39
Hayden Brown <60915164+ScratchCat458@users.noreply.github.com>
2020-08-31 15:53:42 +1000

    Add files via upload

--- src/scratchcat458/luminosity/commands/Help.java
+++ src/scratchcat458/luminosity/commands/Help.java
@@ -1,5 +1,21 @@
 package scratchcat458.luminosity.commands;
 
-public class Help {
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import scratchcat458.luminosity.Default;
 
+public class Help extends ListenerAdapter {
+	public void onCommand(GuildMessageReceivedEvent e) {
+		String args[] = e.getMessage().getContentRaw().split("\\s+");	
+			if(args[0].equalsIgnoreCase(Default.prefix + "help")) {
+				EmbedBuilder help = new EmbedBuilder();
+				help.setTitle("Commands");
+				help.addField("Information:", "`" + Default.prefix + "info`", false);
+				help.addField("Sudo:", "`" + Default.prefix + "sudo [some-random-text]`", false);
+				help.addField("Bot Status:", "`" + Default.prefix + "status [type] [status|some-random-text]`", false);
+				
+				e.getGuild().getDefaultChannel().sendMessage(help.build()).queue();
+			}
+		}
 }
--- src/scratchcat458/luminosity/commands/Sudo.java
+++ src/scratchcat458/luminosity/commands/Sudo.java
@@ -1,6 +1,5 @@
 package scratchcat458.luminosity.commands;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import scratchcat458.luminosity.Default;
@@ -8,15 +7,8 @@ import scratchcat458.luminosity.Default;
 public class Sudo extends ListenerAdapter {
 	public void onCommand(GuildMessageReceivedEvent e) {
 		String args[] = e.getMessage().getContentRaw().split("\\s+");	
-			if(args[0].equalsIgnoreCase(Default.prefix + "info")) {
-				EmbedBuilder help = new EmbedBuilder();
-				help.setTitle("Command Help");
-				help.setDescription("All Luminosity commands and their usage.");
-				help.addField("Bot Name:", "Luminosity", false);
-				help.addField("Developer:", "ScratchCat458", false);
-				help.addField("Hosting:", "localhost", false);
-				
-				e.getGuild().getDefaultChannel().sendMessage(help.build()).queue();
+			if(args[0].equalsIgnoreCase(Default.prefix + "sudo")) {
+				e.getGuild().getDefaultChannel().sendMessage(args[1].replace("-", " ")).queue();
 			}
 		}
 }
