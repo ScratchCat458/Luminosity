4138bde3e8dc9b637f31b35c7fa27b22a03efcb6
Hayden Brown <60915164+ScratchCat458@users.noreply.github.com>
2020-09-05 15:05:13 +1000

    Update Default.java

--- src/scratchcat458/luminosity/Default.java
+++ src/scratchcat458/luminosity/Default.java
@@ -1,7 +1,6 @@
 package scratchcat458.luminosity;
 
 public interface Default {
-	public static String token = "token";
 	public static String prefix = "~";
 	public static String activity = "Impact Client";
 }
